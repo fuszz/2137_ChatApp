6a39cea61ccdaaf85c9dba99a3132cc6d20997fe
fuszz <uliaszfilip@gmail.com>
2024-07-04 08:17:17 +0200

    Modyfikacja models.py

--- models.py
+++ models.py
@@ -13,7 +13,7 @@ class User(UserMixin, db.Model):
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(500), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
